5a3e50bfb1e87b2db01c64a96da3e015d9f5c34d
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-10 11:11:51 -0200

    .

--- Parte 8/01 Sistema de Arquivos/Item_00.cs
+++ Parte 8/01 Sistema de Arquivos/Item_00.cs
@@ -1,33 +1,11 @@
 ﻿using System;
-using System.IO;
-using System.IO.Pipes;
-using System.Net.Sockets;
-using System.Text;
 
 namespace Listings
 {
     class Item_00 //Streams
     {
-        static void XMain(string[] args)
+        static void Main(string[] args)
         {
-            FileStream fluxoArquivo = 
-                new FileStream("Diretores.txt", FileMode.Open, FileAccess.Read);
-
-            byte[] bytes = new byte[10];
-            int posicao = 0;
-            int tamanho = 10;
-            fluxoArquivo.Read(bytes, posicao, tamanho);
-
-            fluxoArquivo.Seek(5, SeekOrigin.Current);
-            fluxoArquivo.Read(bytes, posicao, tamanho);
-
-            foreach (var caractere in bytes)
-            {
-                //Console.Write(caractere);
-                Console.Write((char)caractere);
-            }
-
-            Console.ReadKey();
         }
     }
 }
