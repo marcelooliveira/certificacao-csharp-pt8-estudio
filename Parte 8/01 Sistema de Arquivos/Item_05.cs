6cbc193dd2670a2929469220590e38e898f8de29
marcelooliveira <mclricardo@gmail.com>
2018-12-10 17:38:10 -0200

    .

--- Parte 8/01 Sistema de Arquivos/Item_05.cs
+++ Parte 8/01 Sistema de Arquivos/Item_05.cs
@@ -5,28 +5,13 @@ using System.Text;
 
 namespace Listings
 {
-    class Item_05 //The File class
+    class Item_05 //A classe File
     {
         static void XMain(string[] args)
         {
-            File.WriteAllText(path: "Arquivo.txt", contents: "Conteúdo inicial do arquivo");
-            File.AppendAllText(path: "Arquivo.txt", contents: "- conteúdo adicionado depois");
+            //TAREFAS: GRAVAR E LER DADOS DE UM ARQUIVO USANDO A CLASSE File
 
-            if (File.Exists("Arquivo.txt"))
-                Console.WriteLine("Arquivo já existe");
-
-            string conteudo = File.ReadAllText(path: "Arquivo.txt");
-
-            Console.WriteLine("Conteúdo do arquivo: {0}", conteudo);
-            File.Copy(sourceFileName: "Arquivo.txt", destFileName: "CopiaArquivo.txt");
-
-            using (TextReader leitor = File.OpenText(path: "CopiaArquivo.txt"))
-            {
-                string texto = leitor.ReadToEnd();
-                Console.WriteLine("Texto copiado: {0}", texto);
-            }
-
-            Console.ReadKey();
+            
         }
     }
 }
