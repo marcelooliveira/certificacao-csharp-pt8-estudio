bc80e61878f68c3461ea0cc1fe62a10a1257d75a
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-11 11:50:04 -0200

    1078-CertificacaoCSharpParte8-video2.3-Obtendo Informações Sobre os Drives.trec

--- Parte 8/01 Sistema de Arquivos/Item_07.cs
+++ Parte 8/01 Sistema de Arquivos/Item_07.cs
@@ -5,6 +5,8 @@ namespace Listings
 {
     class Item_07 //Informações do drive
     {
+        private const int KILOBYTE = 1024;
+
         static void Main(string[] args)
         {
             //TAREFA:
@@ -13,7 +15,40 @@ namespace Listings
             //Verificar se o drive está pronto
             //Tipo do drive
             //Formato do drive
-            //Espaço livre, em bytes, MB, GB e TB
+            //Espaço livre, em bytes, KB, MB, GB e TB
+
+            var drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                Console.WriteLine("Nome: {0}", drive.Name);
+                if (!drive.IsReady)
+                {
+                    Console.WriteLine("O drive não está pronto.");
+                    continue;
+                }
+                Console.WriteLine("Tipo: {0}", drive.DriveType);
+                Console.WriteLine("Formato: {0}", drive.DriveFormat);
+
+                Console.WriteLine("Espaço livre:");
+
+                long bytes = drive.TotalFreeSpace;
+                Console.WriteLine("{0} bytes", bytes);
+
+                double kb = bytes / KILOBYTE;
+                Console.WriteLine("{0:N2} KB", kb);
+
+                double mb = kb / KILOBYTE;
+                Console.WriteLine("{0:N2} MB", mb);
+
+                double gb = mb / KILOBYTE;
+                Console.WriteLine("{0:N2} GB", gb);
+
+                double tb = gb / KILOBYTE;
+                Console.WriteLine("{0:N2} TB", tb);
+
+                Console.WriteLine();
+            }
         }
     }
 }
