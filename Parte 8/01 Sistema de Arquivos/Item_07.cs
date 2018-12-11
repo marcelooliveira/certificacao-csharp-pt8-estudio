84b4e0a2cec47168942dc981fa6480379317d950
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-11 11:16:36 -0200

    .

--- Parte 8/01 Sistema de Arquivos/Item_07.cs
+++ Parte 8/01 Sistema de Arquivos/Item_07.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Listings
 {
@@ -6,7 +7,13 @@ namespace Listings
     {
         static void Main(string[] args)
         {
-
+            //TAREFA:
+            //=======
+            //Nome do drive
+            //Verificar se o drive está pronto
+            //Tipo do drive
+            //Formato do drive
+            //Espaço livre, em bytes, MB, GB e TB
         }
     }
 }
