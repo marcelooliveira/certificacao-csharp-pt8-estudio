a9141007b188f115907aaa2f6edec6c5840131a0
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-12 11:02:36 -0200

    1078-CertificacaoCSharpParte8-video2.4-Informações do Arquivo.trec

--- Parte 8/01 Sistema de Arquivos/Item_08.cs
+++ Parte 8/01 Sistema de Arquivos/Item_08.cs
@@ -9,6 +9,7 @@ namespace Listings
     {
         static void Main(string[] args)
         {
+            const string caminho = "Arquivo.txt";
             //TAREFA:
             //1. Gravar um texto em Arquivo.txt
             //2. Obter informações desse arquivo:
@@ -21,6 +22,18 @@ namespace Listings
             //      Verificar os atributos novamente
             //      Remover atributo somente-leitura
             //      Verificar os atributos novamente
+
+            File.WriteAllText(caminho, "Texto inicial do arquivo");
+            FileInfo info = new FileInfo(caminho);
+            Console.WriteLine("Nome {0}", info.Name);
+            Console.WriteLine("Caminho completo: {0}", info.FullName);
+            Console.WriteLine("Último acesso: {0}", info.LastAccessTime);
+            Console.WriteLine("Tamanho: {0} bytes", info.Length);
+            Console.WriteLine("Atributos: {0}", info.Attributes);
+            info.Attributes = info.Attributes | FileAttributes.ReadOnly;
+            Console.WriteLine("Atributos: {0}", info.Attributes);
+            info.Attributes = info.Attributes & ~FileAttributes.ReadOnly;
+            Console.WriteLine("Atributos: {0}", info.Attributes);
         }
     }
 }
