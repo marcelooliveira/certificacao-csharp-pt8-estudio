ea2360f7e8f06324ddc1031accb81e3a73e574ca
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-12 12:44:11 -0200

    1078-CertificacaoCSharpParte8-video2.8-Procurando Arquivos Recursivamente.trec

--- Parte 8/01 Sistema de Arquivos/Item_12.cs
+++ Parte 8/01 Sistema de Arquivos/Item_12.cs
@@ -13,6 +13,24 @@ namespace Listings
             //Obter o diretório de início do projeto
             //Listar todos os diretórios do projeto
             //Listar todos os arquivos csharp (.cs) do projeto
+
+            DirectoryInfo diretorioInicial = new DirectoryInfo(@"..\..\..");
+            ListarDiretorios(diretorioInicial);
+        }
+
+        private static void ListarDiretorios(DirectoryInfo diretorioInicial)
+        {
+            foreach (var diretorio in diretorioInicial.GetDirectories())
+            {
+                Console.WriteLine(diretorio.FullName);
+
+                foreach (var arquivo in diretorio.GetFiles("*.cs"))
+                {
+                    Console.WriteLine(arquivo.FullName);
+                }   
+
+                ListarDiretorios(diretorio);
+            }
         }
     }
 }
