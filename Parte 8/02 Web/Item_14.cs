245e94f85de3e8930842d696ce9e8e8f287cf2ca
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-13 17:19:41 -0200

    1078-CertificacaoCSharpParte8-video3.2-Trabalhando com WebClient.trec

--- Parte 8/02 Web/Item_14.cs
+++ Parte 8/02 Web/Item_14.cs
@@ -13,6 +13,11 @@ namespace Listings
             //Conectar-se site da caelum (http://www.caelum.com.br)
             //apenas para obter e exibir o conteúdo da página do site
             //mas de forma mais simples e rápida
+
+            WebClient webClient = new WebClient();
+            string textoDoSite = webClient.DownloadString("http://www.caelum.com.br");
+            Console.WriteLine(textoDoSite);
+            Console.ReadKey();
         }
     }
 }
