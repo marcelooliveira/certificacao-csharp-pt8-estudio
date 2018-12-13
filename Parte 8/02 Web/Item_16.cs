4b529eb8613c2720bb5aaea36bda0a1fbc74f1fc
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-13 18:30:32 -0200

    .

--- Parte 8/02 Web/Item_16.cs
+++ Parte 8/02 Web/Item_16.cs
@@ -13,7 +13,12 @@ namespace Listings
             //TAREFA:
             //Conectar-se site da caelum (http://www.caelum.com.br)
             //de forma ASSÍNCRONA, porém o código precisa rodar em 
-            //Windows Universal Application Platform
+            // 
+            // - Aplicações Windows (Windows Forms, WPF)
+            // - Aplicações Web (ASP.NET e ASP.NET Core)
+            // - Xamarin (aplicativos de celular / tablet)
+            // - Windows Universal Application Platform
+
         }
     }
 }
