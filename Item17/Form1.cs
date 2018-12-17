6bc16038b978d81aaef0af0c01200e75c83b7b0b
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-17 14:48:28 -0200

    .

---
+++ Item17/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Item17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtTimer.Text = DateTime.Now.ToString("HH:mm:ss:fff");
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
