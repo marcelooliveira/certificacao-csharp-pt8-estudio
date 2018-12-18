39282ef8f4a86ced10d3dbc2d02d683c129e9010
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-18 17:17:16 -0200

    .

--- Parte 8/05 JSON e XML/Item_25.cs
+++ Parte 8/05 JSON e XML/Item_25.cs
@@ -9,42 +9,13 @@ namespace Listings
 {
     class Item_25 //Json
     {
-        static async Task XMain(string[] args)
+        static void Main(string[] args)
         {
-            string cep = "04101300";
-            string url = $"http://viacep.com.br/ws/{cep}/json/";
+            //TAREFA:
+            //CONSULTAR UM CEP NO SITE http://viacep.com.br
+            //E EXIBIR SEUS DADOS
 
-            var endereco = await GetEndereco(url);
-            Console.WriteLine($"{endereco.logradouro} - {endereco.bairro} - {endereco.localidade} - {endereco.uf}");
             Console.ReadKey();
-        }
-
-        static async Task<Endereco> GetEndereco(string url)
-        {
-            string json = await LerPaginaWeb(url);
-            return JsonConvert.DeserializeObject<Endereco>(json);
-        }
-
-        static async Task<string> LerPaginaWeb(string uri)
-        {
-            HttpClient cliente = new HttpClient();
-            return await cliente.GetStringAsync(uri);
-        }
-
-        //gerado por: http://json2csharp.com/
-
-        //public class RootObject
-        public class Endereco
-        {
-            public string cep { get; set; }
-            public string logradouro { get; set; }
-            public string complemento { get; set; }
-            public string bairro { get; set; }
-            public string localidade { get; set; }
-            public string uf { get; set; }
-            public string unidade { get; set; }
-            public string ibge { get; set; }
-            public string gia { get; set; }
         }
     }
 }
