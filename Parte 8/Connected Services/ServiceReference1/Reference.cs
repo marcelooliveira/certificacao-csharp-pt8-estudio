117c99ef6e0c0734dd2922888305b80096b929bd
Marcelo Oliveira <mclricardo@gmail.com>
2018-12-19 16:42:21 -0200

    1078-CertificacaoCSharpParte8-Video6.3-Consumindo Serviço WCF.trec

---
+++ Parte 8/Connected Services/ServiceReference1/Reference.cs
@@ -0,0 +1,127 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//     //
+//     As alterações no arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for gerado novamente.
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ServiceReference1
+{
+    
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.1")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="ServiceReference1.ICursoService")]
+    public interface ICursoService
+    {
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/ICursoService/GetValor", ReplyAction="http://tempuri.org/ICursoService/GetValorResponse")]
+        System.Threading.Tasks.Task<string> GetValorAsync(int chave);
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.1")]
+    public interface ICursoServiceChannel : ServiceReference1.ICursoService, System.ServiceModel.IClientChannel
+    {
+    }
+    
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.1")]
+    public partial class CursoServiceClient : System.ServiceModel.ClientBase<ServiceReference1.ICursoService>, ServiceReference1.ICursoService
+    {
+        
+    /// <summary>
+    /// Implemente este método parcial para configurar o ponto de extremidade de serviço.
+    /// </summary>
+    /// <param name="serviceEndpoint">O ponto de extremidade a ser configurado</param>
+    /// <param name="clientCredentials">As credenciais do cliente</param>
+    static partial void ConfigureEndpoint(System.ServiceModel.Description.ServiceEndpoint serviceEndpoint, System.ServiceModel.Description.ClientCredentials clientCredentials);
+        
+        public CursoServiceClient() : 
+                base(CursoServiceClient.GetDefaultBinding(), CursoServiceClient.GetDefaultEndpointAddress())
+        {
+            this.Endpoint.Name = EndpointConfiguration.BasicHttpBinding_ICursoService.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public CursoServiceClient(EndpointConfiguration endpointConfiguration) : 
+                base(CursoServiceClient.GetBindingForEndpoint(endpointConfiguration), CursoServiceClient.GetEndpointAddress(endpointConfiguration))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public CursoServiceClient(EndpointConfiguration endpointConfiguration, string remoteAddress) : 
+                base(CursoServiceClient.GetBindingForEndpoint(endpointConfiguration), new System.ServiceModel.EndpointAddress(remoteAddress))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public CursoServiceClient(EndpointConfiguration endpointConfiguration, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(CursoServiceClient.GetBindingForEndpoint(endpointConfiguration), remoteAddress)
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public CursoServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(binding, remoteAddress)
+        {
+        }
+        
+        public System.Threading.Tasks.Task<string> GetValorAsync(int chave)
+        {
+            return base.Channel.GetValorAsync(chave);
+        }
+        
+        public virtual System.Threading.Tasks.Task OpenAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
+        }
+        
+        public virtual System.Threading.Tasks.Task CloseAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetBindingForEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_ICursoService))
+            {
+                System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+                result.MaxBufferSize = int.MaxValue;
+                result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+                result.MaxReceivedMessageSize = int.MaxValue;
+                result.AllowCookies = true;
+                return result;
+            }
+            throw new System.InvalidOperationException(string.Format("Não foi possível encontrar o ponto de extremidade com o nome \'{0}\'.", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_ICursoService))
+            {
+                return new System.ServiceModel.EndpointAddress("http://localhost:50290/CursoService.svc");
+            }
+            throw new System.InvalidOperationException(string.Format("Não foi possível encontrar o ponto de extremidade com o nome \'{0}\'.", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetDefaultBinding()
+        {
+            return CursoServiceClient.GetBindingForEndpoint(EndpointConfiguration.BasicHttpBinding_ICursoService);
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetDefaultEndpointAddress()
+        {
+            return CursoServiceClient.GetEndpointAddress(EndpointConfiguration.BasicHttpBinding_ICursoService);
+        }
+        
+        public enum EndpointConfiguration
+        {
+            
+            BasicHttpBinding_ICursoService,
+        }
+    }
+}
